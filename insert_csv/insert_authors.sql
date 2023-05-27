6b23c25de5035fd59021a632f8a844cd9802b4cf
Panagiotis Papastathis <118458162+PanagitisPapastathis@users.noreply.github.com>
2023-05-27 22:16:36 +0300

    Create insert_authors.sql

---
+++ insert_csv/insert_authors.sql
@@ -0,0 +1,34 @@
+insert into Author(Author_id, Name) values
+(1, 'Penelope Delta'),
+(2, 'George R.R. Martin'),
+(3, 'J.R.R. Tolkien'),
+(4, 'Jules Verne'),
+(5, 'Fyodor Dostoevsky'),
+(6, 'Thomas Mann'),
+(7, 'Friedrich Nietzsche'),
+(8, 'Boris Vian'),
+(9, 'Julio Cortázar'),
+(10, 'Fernando Aramburu'),
+(12, 'William Shakespeare'),
+(13, 'Jane Austen'),
+(14, 'Charles Dickens'),
+(15, 'George Orwell'),
+(16, 'Virginia Woolf'),
+(17, 'Agatha Christie'),
+(18, 'William Wordsworth'),
+(19, 'Emily Brontë'),
+(20, 'Oscar Wilde'),
+(21, 'Lewis Carroll'),
+(22, 'D.H. Lawrence'),
+(23, 'Bram Stoker'),
+(24, 'Aldous Huxley'),
+(25, 'Arthur Conan Doyle'),
+(26, 'George Eliot'),
+(27, 'Thomas Hardy'),
+(28, 'H.G. Wells'),
+(29, 'Rudyard Kipling'),
+(30, 'J.K. Rowling');
+
+
+
+
