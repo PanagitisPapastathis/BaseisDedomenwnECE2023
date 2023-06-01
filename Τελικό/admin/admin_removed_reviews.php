a20ba155ef9601cea6c2e1ae9d38811847042af8
Sotiris Gkiouleas <78240859+klarinetos@users.noreply.github.com>
2023-06-01 17:29:33 +0300

    Update admin_removed_reviews.php

--- Τελικό/admin/admin_removed_reviews.php
+++ Τελικό/admin/admin_removed_reviews.php
@@ -40,7 +40,7 @@
                         error_reporting(E_ALL);
                         ini_set('display_errors', '1');
                         
-                        include 'connection.php';
+                        include '../connection.php';
                         $conn = OpenCon();
 
                         $query = "SELECT * FROM Accept_Reviews_Help WHERE Status='Removed'"; 
