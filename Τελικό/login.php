656e3ab6c45f467dad03e805b396d0366827375c
Panagiotis Papastathis <118458162+PanagitisPapastathis@users.noreply.github.com>
2023-06-01 15:01:47 +0300

    Add files via upload

---
+++ Τελικό/login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+<h2> Login Page </h2>
+<p>
+    <form method="post" action="login.php">
+    <input type="text" name="username" placeholder="Username" required><br><br>
+    <input type="password" name="password" placeholder="Password" required><br><br>
+    <input type="submit" value="Login"> <br> <br>
+    <?php
+        include 'connection.php';
+        $conn = OpenCon();
+
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+    
+        session_start();
+        $_SESSION["username"] = $username;
+        
+        $query = "SELECT * FROM Users WHERE username = '$username'";
+        $result = mysqli_query($conn, $query);
+
+        if ($row = mysqli_fetch_assoc($result)) {
+            $storedPassword = $row['Password'];
+            $status = $row['Status'];
+        
+            if ($password == $storedPassword) {
+                // Redirect based on user's status
+                switch ($status) {
+                    case 'Student':
+                        header("Location: ./Student/student.php");
+                        break;
+                    case 'Teacher':
+                        header("Location: ./Teacher/teacher.php");
+                        break;
+                    case 'Admin':
+                        header("Location: ./Admin/admin.php");
+                        break;
+                    case 'Central Admin':
+                        header("Location: ./Central_admin/central_admin.php");
+                        break;
+                    default:
+                        echo "Invalid user status.";
+                }
+                exit; // Make sure to exit after redirecting
+            } else {
+                echo "Incorrect password.";
+            }
+        } else {
+            echo "User does not exist.";
+        }
+        $conn -> close();
+    ?>
+</form>
+</p>
+    <?php if(isset($error)) { echo $error; } ?>
+</body>
+</html>
