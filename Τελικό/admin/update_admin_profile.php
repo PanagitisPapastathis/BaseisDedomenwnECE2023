d419f98fdd5984ec5a786dec388c5477fc8ec7a6
Sotiris Gkiouleas <78240859+klarinetos@users.noreply.github.com>
2023-06-01 17:46:46 +0300

    Update update_admin_profile.php

--- Τελικό/admin/update_admin_profile.php
+++ Τελικό/admin/update_admin_profile.php
@@ -5,7 +5,7 @@
     <meta charset = "utf-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>
-        School Unit Administrator Profile Update
+        Profile Update
     </title>
     <link rel = "stylesheet" href = "css/styles.css">
     <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
