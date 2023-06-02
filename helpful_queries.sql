d3bcbbe50b12b3bd3f5bd38de45b8f0a62574f5a
Sotiris Gkiouleas <78240859+klarinetos@users.noreply.github.com>
2023-06-02 20:08:39 +0300

    FANHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- helpful_queries.sql
+++ helpful_queries.sql
@@ -8,3 +8,13 @@ FROM Subject AS sb
 JOIN Book_Subject AS bs ON sb.Subject_id = bs.Subject_id
 JOIN Reviews AS r ON bs.ISBN = r.ISBN
 GROUP BY sb.Subject_name;
+
+CREATE VIEW IF NOT EXISTS Admin_Lendings_count AS
+SELECT User.Username, COUNT(Lending.Serial_number) AS No_of_Lendings
+FROM Users JOIN Lending ON Users.Username = Lending.Approved_by
+GROUP BY User.Username
+HAVING No_of_Lendings>=22;
+
+SELECT adm1.Username AS adm1_Username, adm2.Username AS adm2_Username, adm1.No_of_Lendings
+FROM Admin_Lendings_count AS adm1 JOIN Admin_Lendings_count AS adm2
+ON adm1.No_of_Lendings = adm2.No_of_Lendings AND adm1.Username < adm2.Username;
